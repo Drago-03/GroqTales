cbf3e1fd65a571ad0e70c7bd7dcb00e139571f13
Mantej <hitmanagent404@gmail.com>
2025-08-05 20:28:43 +0530

    lines trim error resolved

--- app/nft-gallery/page.tsx
+++ app/nft-gallery/page.tsx
@@ -513,11 +513,12 @@ export default function NFTGalleryPage() {
 
   const sortedNFTs = [...filteredNFTs].sort((a, b) => {
     switch (sortBy) {
-      case 'price':
+      case 'price': {
         // Extract numeric value from price strings (remove "ETH" and other non-numeric characters)
         const priceA = parseFloat(a.price.replace(/[^\d.]/g, '')) || 0;
         const priceB = parseFloat(b.price.replace(/[^\d.]/g, '')) || 0;
         return priceB - priceA;
+      }
       case 'likes':
         return b.likes - a.likes;
       case 'recent':
